d083cb56c1373a2bbd8c2360962fa67d1bd6dd50
Joseph <josephshonko99@gmail.com>
2024-03-18 22:08:41 +0300

    commiting variable Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName ='Scuber';
+let mostProfitableNeighborhood = 'companyName';
+let companyCeo = 'mostprofitableNeighbourhood';
+let chelsea = 'companyName';
+mostProfitableNeighborhood = 'Chelsea';
+companyCeo ='Susan Smith'
+
